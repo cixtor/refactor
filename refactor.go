b8c0d887e7ab649519f1cf118993415731ad735a
Cixtor <cixtords@gmail.com>
2017-04-06 09:00:00 -0500

    Add method to create new instances of the main interface

--- refactor.go
+++ refactor.go
@@ -27,3 +27,34 @@ type Match struct {
 	GrepFormat string
 	LineNumber int
 }
+
+func main() {
+	flag.Usage = func() {
+		fmt.Println("Refactor")
+		fmt.Println()
+		fmt.Println("Searches all the files in the current directory containing")
+		fmt.Println("the [old] and replacement every occurrence with [new]. Be ")
+		fmt.Println("aware that forward slashes must be escaped in both cases.")
+		fmt.Println()
+		fmt.Println("Usage:")
+		fmt.Println("  refactor [old-text] [new-text]")
+		fmt.Println("  refactor [old-text] [new-text] [files]")
+		os.Exit(2)
+	}
+
+	flag.Parse()
+
+	app := NewRefactor(flag.Arg(0), flag.Arg(1), flag.Args())
+
+	if err := app.Execute(); err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func NewRefactor(oldtext string, newtext string, filelist []string) *Refactor {
+	return &Refactor{
+		Oldtext:  oldtext,
+		Newtext:  newtext,
+		Filelist: filelist,
+	}
+}
